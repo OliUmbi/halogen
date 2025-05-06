bae700af226e6dc5f3570e656fa8d382843e7d7b
oliUmbi <oliver.umbricht@hispeed.ch>
2025-05-06 23:30:58 +0200

    added compression calculation

--- src/styling/styling.go
+++ src/styling/styling.go
@@ -9,47 +9,47 @@ import (
 )
 
 type color struct {
-	Name string
+	Name  string
 	Value string
 }
 
 type breakpoint struct {
-	Name string
+	Name  string
 	Value string
 }
 
 type pseudo struct {
-	Name string
+	Name  string
 	Value string
 }
 
 type variant struct {
-	Name string
+	Name  string
 	Value string
 }
 
 type attribute struct {
-	Name string
+	Name     string
 	Variants []variant
 }
 
 type style struct {
-	Colors []color
+	Colors      []color
 	Breakpoints []breakpoint
-	Pseudos []pseudo
-	Attributes []attribute
+	Pseudos     []pseudo
+	Attributes  []attribute
 }
 
 func main() {
 
 	var data, err = os.ReadFile("./definitions.json")
 	if err != nil {
-			panic(err)
+		panic(err)
 	}
 
 	var style style
 	if err := json.Unmarshal(data, &style); err != nil {
-			panic(err)
+		panic(err)
 	}
 
 	var results []string
@@ -59,28 +59,27 @@ func main() {
 		for _, variant := range attribute.Variants {
 
 			var classes []string
-			classes = append(classes, "." + attribute.Name + "\\=" + variant.Name)
-			
+			classes = append(classes, "."+attribute.Name+"\\="+variant.Name)
+
 			for _, pseudo := range style.Pseudos {
-				classes = append(classes, "." + attribute.Name + "\\:" + pseudo.Name + "\\=" + variant.Name + ":" + pseudo.Value)
+				classes = append(classes, "."+attribute.Name+"\\:"+pseudo.Name+"\\="+variant.Name+":"+pseudo.Value)
 			}
 
-			results = append(results, strings.Join(classes, ",") + "{" + variant.Value + "}")
+			results = append(results, strings.Join(classes, ",")+"{"+variant.Value+"}")
 
 			for _, breakpoint := range style.Breakpoints {
-				
-				breakpoints[breakpoint.Value] = append(breakpoints[breakpoint.Value], "." + attribute.Name + "\\:" + breakpoint.Name + "\\=" + variant.Name + "{" + variant.Value + "}")
+
+				breakpoints[breakpoint.Value] = append(breakpoints[breakpoint.Value], "."+attribute.Name+"\\:"+breakpoint.Name+"\\="+variant.Name+"{"+variant.Value+"}")
 			}
 		}
 	}
 
 	for value, data := range breakpoints {
-		results = append(results, "@media(width<=" + value + "){" + strings.Join(data, "") + "}")
+		results = append(results, "@media(width<="+value+"){"+strings.Join(data, "")+"}")
 	}
 
 	fmt.Println(utf8.RuneCountInString(strings.Join(results, "")))
+	fmt.Println(utf8.RuneCountInString(strings.Join(results, "")) / 8)
 
 	os.WriteFile("./output.css", []byte(strings.Join(results, "")), 0644)
-
 }
-
